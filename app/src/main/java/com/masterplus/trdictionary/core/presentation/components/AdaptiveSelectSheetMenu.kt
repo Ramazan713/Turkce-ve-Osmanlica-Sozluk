299eb96720924422d5781954c673858ace833b35
Ramazan Sengul <ramazansengul137@gmail.com>
2023-10-31 18:05:12 +0300

    add adaptiveSelectSheetMenu component

---
+++ app/src/main/java/com/masterplus/trdictionary/core/presentation/components/AdaptiveSelectSheetMenu.kt
@@ -0,0 +1,165 @@
+package com.masterplus.trdictionary.core.presentation.components
+
+import android.os.Parcelable
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import com.masterplus.trdictionary.R
+import com.masterplus.trdictionary.core.domain.enums.IMenuItemEnum
+import com.masterplus.trdictionary.core.presentation.selectors.CustomDropdownBarMenu
+import com.masterplus.trdictionary.core.presentation.selectors.SelectMenuItemBottomContent
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class AdaptiveSelectMenuData<T> (
+    val items: List<T>,
+    val onItemChange: ((T)->Unit)?,
+    val sheetTitle: String?
+): Parcelable where T : IMenuItemEnum, T: Enum<T>
+
+
+
+@Composable
+fun <T> rememberAdaptiveSelectMenuState(
+    windowWidthSizeClass: WindowWidthSizeClass,
+    hideSheetAfterMenuClick: Boolean = true
+): AdaptiveSelectMenuState<T> where T : IMenuItemEnum, T: Enum<T>{
+
+    var visibleSheet by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val isExpanded by remember(windowWidthSizeClass) {
+        derivedStateOf {
+            windowWidthSizeClass == WindowWidthSizeClass.Expanded
+        }
+    }
+
+    var data by rememberSaveable {
+        mutableStateOf<AdaptiveSelectMenuData<T>?>(null)
+    }
+
+    val handler = remember(isExpanded,visibleSheet,data,hideSheetAfterMenuClick) {
+        AdaptiveSelectMenuState<T>(
+            isExpanded = isExpanded,
+            visibleSheet = visibleSheet,
+            onVisibleSheet = { visibleSheet = it },
+            data = data,
+            setData = { data = it },
+            hideSheetAfterMenuClick = hideSheetAfterMenuClick
+        )
+    }
+    return handler
+}
+
+data class AdaptiveSelectMenuState<T>(
+    private val hideSheetAfterMenuClick: Boolean,
+    val isExpanded: Boolean,
+    private val visibleSheet: Boolean,
+    private val onVisibleSheet: (Boolean) -> Unit,
+    private val data: AdaptiveSelectMenuData<T>?,
+    private val setData: (AdaptiveSelectMenuData<T>?) -> Unit
+) where T : IMenuItemEnum, T: Enum<T>{
+
+    fun setSheetData(
+        data: AdaptiveSelectMenuData<T>,
+    ){
+        onVisibleSheet(true)
+        setData(data)
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    val showSheet: @Composable() () -> Unit  @Composable get() {
+        return {
+            if(visibleSheet){
+                data?.let { data->
+                    CustomModalBottomSheet(
+                        onDismissRequest = { onVisibleSheet(false) },
+                        skipHalfExpanded = true
+                    ){
+                        SelectMenuItemBottomContent<T>(
+                            title = data.sheetTitle,
+                            items = data.items,
+                            onClickItem = {selected->
+                                data.onItemChange?.let {
+                                    if (selected != null) {
+                                        it(selected)
+                                        if(hideSheetAfterMenuClick){
+                                            onVisibleSheet(false)
+                                        }
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun <T> AdaptiveSelectSheetMenu(
+    modifier: Modifier = Modifier,
+    state: AdaptiveSelectMenuState<T>,
+    onItemChange: ((T)->Unit)? = null,
+    items: List<T>,
+    sheetTitle: String? = null,
+    icon: ImageVector = Icons.Default.MoreVert,
+    contentDescription: String? = stringResource(id = R.string.menu),
+    tooltip: String? = contentDescription
+) where T : IMenuItemEnum, T: Enum<T> {
+
+    var visibleDropdown by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(state.isExpanded){
+        if(!state.isExpanded && visibleDropdown){
+            visibleDropdown = false
+        }
+    }
+
+    if(state.isExpanded){
+        CustomDropdownBarMenu(
+            tooltip = tooltip,
+            modifier = modifier,
+            items = items,
+            onItemChange = onItemChange,
+            expanded = visibleDropdown,
+            onExpandedChange = { visibleDropdown = it },
+            icon = icon,
+            contentDescription = contentDescription
+        )
+    }else{
+        DefaultToolTip(tooltip = tooltip) {
+            IconButton(
+                modifier = modifier,
+                onClick = {
+                    state.setSheetData(AdaptiveSelectMenuData(items,onItemChange, sheetTitle))
+                }
+            ){
+                Icon(icon, contentDescription = contentDescription)
+            }
+        }
+    }
+}
--- app/src/main/java/com/masterplus/trdictionary/core/presentation/selectors/CustomDropdownBarMenu.kt
+++ app/src/main/java/com/masterplus/trdictionary/core/presentation/selectors/CustomDropdownBarMenu.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,12 +15,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.masterplus.trdictionary.core.domain.enums.IMenuItemEnum
 import com.masterplus.trdictionary.R
+import com.masterplus.trdictionary.core.presentation.components.DefaultToolTip
 
 
 @Composable
@@ -26,25 +30,57 @@ fun <T: IMenuItemEnum> CustomDropdownBarMenu(
     modifier: Modifier = Modifier,
     items: List<T>,
     onItemChange: ((T)->Unit)? = null,
+    icon: ImageVector = Icons.Default.MoreVert,
+    contentDescription: String? = stringResource(id = R.string.menu),
+    tooltip: String? = contentDescription
+){
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    CustomDropdownBarMenu(
+        modifier = modifier,
+        items = items,
+        onItemChange = onItemChange,
+        icon = icon,
+        contentDescription = contentDescription,
+        tooltip = tooltip,
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = it
+        }
+    )
+}
+
+
+@Composable
+fun <T: IMenuItemEnum> CustomDropdownBarMenu(
+    modifier: Modifier = Modifier,
+    items: List<T>,
+    onItemChange: ((T)->Unit)? = null,
+    expanded: Boolean = false,
+    onExpandedChange: (Boolean) -> Unit,
+    icon: ImageVector = Icons.Default.MoreVert,
+    contentDescription: String? = stringResource(id = R.string.menu),
+    tooltip: String? = contentDescription
 ){
     val context = LocalContext.current
     val shape = MaterialTheme.shapes.small
 
-    var expanded by rememberSaveable {
-        mutableStateOf(false)
-    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        IconButton(onClick = {expanded = true}, modifier = modifier){
-            Icon(painterResource(R.drawable.ic_baseline_more_vert_24),
-                contentDescription = stringResource(R.string.menu))
+        DefaultToolTip(tooltip = tooltip) {
+            IconButton(onClick = { onExpandedChange(true) }, modifier = modifier){
+                Icon(icon, contentDescription = contentDescription)
+            }
         }
+
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = {expanded = false},
+            onDismissRequest = { onExpandedChange(false) },
             modifier = modifier
                 .background(MaterialTheme.colorScheme.surfaceVariant)
                 .padding(horizontal = 3.dp)
@@ -57,7 +93,7 @@ fun <T: IMenuItemEnum> CustomDropdownBarMenu(
                         textColor = MaterialTheme.colorScheme.onSurfaceVariant
                     ),
                     onClick = {
-                        expanded = false
+                        onExpandedChange(false)
                         onItemChange?.invoke(item)
                     },
                     modifier = Modifier.clip(shape),
