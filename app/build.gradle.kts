2c79d6ade6adeee1156eb007d0ca46977916d322
Ramazan Sengul <ramazansengul137@gmail.com>
2024-06-30 15:13:42 +0300

    refactor build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     alias(libs.plugins.firebase.perf.plugin)
     alias(libs.plugins.android.junit5)
     id("kotlin-parcelize")
+    alias(libs.plugins.room)
 }
 
 val keystorePropertiesFile = rootProject.file("keystore.properties")
@@ -39,24 +40,7 @@ android {
         buildConfigField("String","TTS_CLIENT_ID","\"${keystoreProperties["ttsClientId"]}\"")
         buildConfigField("String","TTS_KEY","\"${keystoreProperties["ttsKey"]}\"")
         buildConfigField("String","INTERSTITIAL_AD_ID","\"${keystoreProperties["interstitialTestAdId"]}\"")
-
-
-        javaCompileOptions {
-            annotationProcessorOptions {
-                arguments += mapOf(
-                    "room.schemaLocation" to "$projectDir/schemas",
-                    "room.incremental" to "true"
-                )
-
-            }
-        }
     }
-
-    ksp {
-        arg(RoomSchemaArgProvider(File(projectDir, "schemas")))
-    }
-
-
     signingConfigs {
 
         create("release"){
@@ -116,9 +100,37 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
+    androidTestImplementation(libs.androidx.uiautomator)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.assertk)
+    androidTestImplementation(libs.mockk.android)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testImplementation(libs.junit.jupiter.params)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.assertk)
+    testImplementation(libs.mockk)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+
+
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
@@ -126,106 +138,49 @@ dependencies {
     implementation(libs.material.icons.core)
     implementation(libs.material.icons.extended)
     implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
-    implementation(libs.androidx.uiautomator)
-    implementation(libs.androidx.navigation.testing)
-    testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.hilt.navigation.compose)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+
     testImplementation(libs.hilt.android.testing)
     kspTest(libs.hilt.android.compiler)
-    androidTestImplementation(libs.dagger.hilt.android.testing)
-    kspAndroidTest(libs.dagger.hilt.android.compiler)
 
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.android.compiler)
 
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    annotationProcessor(libs.room.compiler)
-    ksp(libs.androidx.room.compiler)
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
     testImplementation(libs.room.testing)
-    implementation(libs.androidx.room.paging)
 
-    implementation(libs.androidx.paging.runtime.ktx)
-    implementation(libs.androidx.paging.compose)
-
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-
+    implementation(libs.bundles.lifecycle)
+    ksp(libs.lifecycle.compiler)
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.gson)
 
     implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.auth)
-    implementation(libs.play.services.auth)
-    implementation(libs.firebase.storage)
-    implementation(libs.firebase.crashlytics)
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.config)
-    implementation(libs.firebase.perf)
+    implementation(libs.bundles.firebase)
 
     implementation(libs.coil.compose)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
 
-    implementation(libs.androidx.media3.exoplayer)
-    implementation(libs.androidx.media3.exoplayer.dash)
-    implementation(libs.androidx.media3.ui)
+    implementation(libs.bundles.media3.exoplayer)
+    implementation(libs.bundles.glance)
+    implementation(libs.bundles.paging)
+    implementation(libs.bundles.datastore)
 
     implementation(libs.play.services.ads)
+    implementation(libs.play.services.auth)
     implementation(libs.billing.ktx)
+    implementation(libs.bundles.play.review)
 
     implementation(libs.androidx.core.splashscreen)
-
-    implementation(libs.review)
-    implementation(libs.review.ktx)
 
     implementation(libs.accompanist.adaptive)
     implementation(libs.androidx.material3.window.size)
 
-    implementation(libs.androidx.glance.appwidget)
-    implementation(libs.androidx.glance.material3)
-
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.androidx.datastore.preferences)
-    implementation(libs.androidx.datastore)
-    implementation(libs.androidx.datastore.core.android)
-
-
-    testImplementation(libs.junit.jupiter.api)
-    testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.junit.jupiter.params)
-
-    testImplementation(libs.assertk)
-    testImplementation(libs.mockk)
-    testImplementation(libs.mockwebserver)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.turbine)
-    androidTestImplementation(libs.assertk)
-    androidTestImplementation(libs.mockk.android)
-}
-
-
-
-class RoomSchemaArgProvider(
-    @get:InputDirectory
-    @get:PathSensitive(PathSensitivity.RELATIVE)
-    val schemaDir: File
-) : CommandLineArgumentProvider {
-
-    override fun asArguments(): Iterable<String> {
-        return listOf(
-            "room.schemaLocation=${schemaDir.path}",
-            "room.incremental=true"
-        )
-    }
 }
